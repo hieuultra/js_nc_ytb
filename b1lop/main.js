2497f7c5a32b5e46ad6e5ac3552b29cf8d91ae70
hieuultra <147162531+hieuultra@users.noreply.github.com>
2024-01-18 19:11:21 +0700

    u

---
+++ b1lop/main.js
@@ -0,0 +1,10 @@
+//Map
+//cac phan tu trong map co 2 gia tri : key- value
+const map1 = new Map();
+//them phan tu vao map
+map1.set(0, "so1");
+map1.set("key1", "so1");
+console.log(map1);
+//lay gia tri tu map
+console.log(map1.get('key1'));
+
